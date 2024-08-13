efc8802bb235f15132c4530f181929b3654f7c41
michaelzoub <micacao15@gmail.com>
2024-08-12 21:55:57 -0400

    blog update 2

--- src/app/blog/blogs.tsx
+++ src/app/blog/blogs.tsx
@@ -2,8 +2,8 @@ export const posts = [
     {
         id: 1,
         date: '2024-08-06',
-        name: 'first_post',
-        description: `Art as an escape from system overload. 
+        name: 'post_1',
+        description: ` Art as an escape from system overload. \n
         Lately, I've been reflecting on how as technology develops exponentially, art progresses towards minimalism (sometimes with hidden meanings). In my opinion this is to counter all the noise new tech produces. This provides humans with an escape into an alternate reality or in other words a safe space. The tech space is undeniably thrilling, but it can also be draining. Perhaps I have this opinion because of my immersion in the tech world but I find myself gravitating towards music with slow progressions, minimalistic but well made melodies and repetitive drum sequences, which let me experience a calm atmosphere. I also enjoy abstract art since it allows me to project my own experiences onto it, which reflect my life. It being simple lets us assign our own meaning to art.`
     },
 ]
