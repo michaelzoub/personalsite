4634c4244ecace834199e933b237f4c50665d523
michaelzoub <micacao15@gmail.com>
2024-08-12 21:53:31 -0400

    slight modification to blog text

--- src/app/blog/blogs.tsx
+++ src/app/blog/blogs.tsx
@@ -3,6 +3,7 @@ export const posts = [
         id: 1,
         date: '2024-08-06',
         name: 'first_post',
-        description: `This is something I've been thinking a bit about recently. As technology develops exponentially, art progresses towards minimalism (sometimes with hidden meanings), in my opinion this is to counter all the noise new tech produces. This provides humans with an escape into an alternate reality or in other words a safe place. The tech space can be very exciting but sometimes draining. Maybe I have this opinion because of my presence in the tech world but I tend to find myself listening to songs with slow progressions, minimalistic but well made melodies and repetitive drum sequences. I also enjoy abstract art because it lets me experience things that reflect my life, it being simple lets us assign our own meaning to art.`
+        description: `Art as an escape from system overload. 
+        Lately, I've been reflecting on how as technology develops exponentially, art progresses towards minimalism (sometimes with hidden meanings). In my opinion this is to counter all the noise new tech produces. This provides humans with an escape into an alternate reality or in other words a safe space. The tech space is undeniably thrilling, but it can also be draining. Perhaps I have this opinion because of my immersion in the tech world but I find myself gravitating towards music with slow progressions, minimalistic but well made melodies and repetitive drum sequences, which let me experience a calm atmosphere. I also enjoy abstract art since it allows me to project my own experiences onto it, which reflect my life. It being simple lets us assign our own meaning to art.`
     },
 ]
