c64fe05efd527b9e6067493881cbe09043aab93a
michaelzoub <micacao15@gmail.com>
2024-08-15 16:39:27 -0400

    bug fix

--- src/app/components/blogs/page.tsx
+++ src/app/components/blogs/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 //not used for now
 
 export default function BlogList() {
-    const [blogposts, setBlogposts] = useState([{id: 0, name: '...', date: '...'}])
+    const [blogposts, setBlogposts] = useState([{id: 0, name: '...', date: '...'},])
 
     let data:any;
     //add checker if hash(password) == value from DB then let access
@@ -23,7 +23,7 @@ export default function BlogList() {
     return (
     <main className="flex h-screen overflow-hidden flex-col items-center p-4 bg-white cursor-default text-black">
         <div className="mt-12 mx-auto rounded-lg border-2 border-gray shadow-inner w-[300px] p-4 divide-y-2 md:w-[500px]">
-            {blogposts.map(e=> <div key={e?.id} className="flex flex-row space-x-14 md:space-x-60 transition delay-50 duration-300 ease-in-out hover:text-orange-400"><Link href={{pathname: e.name, query: {id: e.id}}} className="flex p-3 cursor-default ">{e.name}</Link><div className="flex place-content-end p-3 max-w-fit">{e.date}</div></div>)}
+            {blogposts.map(e=> <div key={e?.id} className="flex flex-row space-x-14 md:space-x-60 transition delay-50 duration-300 ease-in-out hover:text-orange-400"><Link href={{pathname: e.name, query: {id: e.id}}} className="flex p-3 cursor-default ">{e?.name}</Link><div className="flex place-content-end p-3 max-w-fit">{e?.date}</div></div>)}
         </div>
         <Link href="/components/addpost" className="m-4 px-2 rounded-lg shadow-inner border-2">+</Link>
     </main>
