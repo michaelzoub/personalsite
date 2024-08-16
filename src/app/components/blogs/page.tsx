52fd41129b0e6bcf0490334066138bb43adc5390
michaelzoub <micacao15@gmail.com>
2024-08-15 20:45:53 -0400

    updated float

--- src/app/components/blogs/page.tsx
+++ src/app/components/blogs/page.tsx
@@ -26,7 +26,7 @@ export default function BlogList() {
     <main className="flex overflow-hidden flex-col items-center p-4 bg-white cursor-default text-black">
         <div className="mt-12 mx-auto rounded-lg border-2 border-gray shadow-inner w-[300px] p-4 divide-y-2 md:w-[500px]">
             <Suspense fallback={<Loading />}>
-            {blogposts.map(e=> <Link  key={e?.id} href={{pathname: e.name, query: {id: e?.id}}} className="flex-row cursor-default md:transition delay-50 duration-300 ease-in-out hover:text-orange-400"><div className="float-left max-w-fit p-3">{e?.name}</div><div className="float-right p-3 max-w-fit">{e?.date}</div></Link>)}
+            {blogposts.map(e=> <Link  key={e?.id} href={{pathname: e.name, query: {id: e?.id}}} className="flex flex-col flex-wrap cursor-default md:transition delay-50 duration-300 ease-in-out hover:text-orange-400"><div className=""><div className="float-left max-w-fit p-3">{e?.name}</div><div className="float-right p-3 max-w-fit">{e?.date}</div></div></Link>)}
             </Suspense>
         </div>
         <Link href="/components/addpost" className="m-4 px-2 rounded-lg shadow-inner border-2">+</Link>
