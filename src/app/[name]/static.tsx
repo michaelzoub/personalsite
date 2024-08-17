07374f1278247091f890291039bbbd4131437eb6
michaelzoub <micacao15@gmail.com>
2024-08-17 12:12:08 -0400

    Add files via upload

--- src/app/[name]/static.tsx
+++ src/app/[name]/static.tsx
@@ -1,3 +1,7 @@
 export async function generateStaticParams() {
-  return {name: string}[]
+  const posts = await fetch('api/getblogcontent').then((res) => res.json())
+ 
+  return posts.map((post:any) => ({
+    name: post.name,
+  }))
 }
