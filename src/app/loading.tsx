399fac860b4ba3397679dbfdce99455d650b3156
michaelzoub <micacao15@gmail.com>
2024-08-19 17:00:39 -0400

    Add files via upload

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
-        <main className="mt-20 absolute flex flex-col items-center p-4 bg-white cursor-default text-black">
-            <div className="kafkasmall md:kafka"></div>
+        <main className="min-h-screen flex flex-col items-center p-4 bg-white cursor-default text-black">
+            <div className="mt-20 kafkasmall md:kafka"></div>
         </main>
     )
 }
