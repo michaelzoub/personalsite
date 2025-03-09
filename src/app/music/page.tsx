a00475f503e8517026e5c4a7585b253bc78b77f7
1mints <micacao15@gmail.com>
2025-03-09 13:02:33 -0400

    ssr fix

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,12 +1,14 @@
 "use client"
-import { Navbar } from "../components/navbar"
+//import { Navbar } from "../components/navbar"
+import dynamic from "next/dynamic";
+//import MusicGraph from "../components/musicVisual/graph"
 
-import MusicGraph from "../components/musicVisual/graph"
+const MusicPlayer = dynamic(() => import("../components/musicVisual/graph"), { ssr: false });
 
 export default function Test() {
     return (
         <main className="h-screen w-full">
-            <MusicGraph></MusicGraph>
+            <MusicPlayer></MusicPlayer>
         </main>
     )
 }
