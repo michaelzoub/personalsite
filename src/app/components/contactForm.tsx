bcd1d6a598cb4342763a01fe5557819e24ee799a
1mints <micacao15@gmail.com>
2025-03-11 17:45:57 -0400

    added contact simplifiers

--- src/app/components/contactForm.tsx
+++ src/app/components/contactForm.tsx
@@ -16,6 +16,7 @@ export default function ContactForm({ dark }: PropsType) {
     const [subject, setSubject] = useState("");
     const [from, setFrom] = useState("");
     const [text, setText] = useState("");
+    const [isActive, setIsActive] = useState(false)
 
     async function sendEmail(formObject: formObject) {
         if ((subject || from || text) == "") {
@@ -33,11 +34,58 @@ export default function ContactForm({ dark }: PropsType) {
 
     }
 
+    function eraseAll() {
+        setSubject("");
+        setFrom("");
+        setText("");
+    }
+
     return (
         <motion.main className="flex flex-col w-full gap-1">
-            <motion.div className="w-full flex flex-row gap-3"
+            <motion.div className="w-full flex flex-col place-items-end gap-2"
                 initial={{ opacity: 0, y: 25 }}
                 transition={{ delay: 0.1 }}
+                whileInView={{opacity: 1, y: 0}}
+            >
+                <div className="w-full flex flex-row justify-between hidden">
+                    <div>Autocomplete</div>
+                    <div onClick={() => setIsActive(!isActive)} className="cursor-pointer">
+                        <motion.div
+                            className={`relative w-14 h-6 rounded-full flex items-center p-1`}
+                            initial={{ backgroundColor: "#E4E4E7" }}
+                            animate={{
+                            backgroundColor: isActive ? "#F97316" : "#E4E4E7",
+                            }}
+                            transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 30,
+                            }}
+                        >
+                            <motion.div
+                            className="w-4 h-4 bg-white rounded-full"
+                            initial={{ x: 0 }}
+                            animate={{
+                                x: isActive ? 31.5 : 0,
+                            }}
+                            transition={{
+                                type: "spring",
+                                stiffness: 500,
+                                damping: 30,
+                            }}
+                            />
+                        </motion.div>
+        </div>
+                </div>
+                <motion.button className={`${dark ? "bg-zinc-900 text-zinc-400" : "bg-zinc-200"} text-zinc-600 rounded-lg px-2 py-1 w-fit`}
+                    whileHover={{ scale: 1.055, }}
+                    transition={{ type: "spring", duration: 0.3 }}
+                    onClick={eraseAll}
+                >Erase</motion.button>
+            </motion.div>
+            <motion.div className="w-full flex flex-row gap-3"
+                initial={{ opacity: 0, y: 25 }}
+                transition={{ delay: 0.2 }}
                 whileInView={{opacity: 1, y: 0}}
             >
                 <div className="w-full flex flex-col">
@@ -51,7 +99,7 @@ export default function ContactForm({ dark }: PropsType) {
             </motion.div>
             <motion.div className="flex flex-col"
                 initial={{ opacity: 0, y: 32 }}
-                transition={{ delay: 0.2 }}
+                transition={{ delay: 0.3 }}
                 whileInView={{opacity: 1, y: 0}}
             >
                 <div className="text-left">Content</div>
