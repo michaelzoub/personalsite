020d58e3e0075d1de9ccc9a90abed5f2c38307a2
1mints <micacao15@gmail.com>
2025-03-11 22:36:41 -0400

    Minor improvements

--- src/app/components/contactForm.tsx
+++ src/app/components/contactForm.tsx
@@ -30,6 +30,7 @@ export default function ContactForm({ dark }: PropsType) {
             body: JSON.stringify(formObject)
         })
         const data = await response.json();
+        eraseAll();
         console.log(data)
 
     }
@@ -47,14 +48,14 @@ export default function ContactForm({ dark }: PropsType) {
                 transition={{ delay: 0.1 }}
                 whileInView={{opacity: 1, y: 0}}
             >
-                <div className="w-full flex flex-row justify-between hidden">
-                    <div>Autocomplete</div>
+                <div className="hidden w-full flex flex-row justify-between">
+                    <div>Autocomplete   -------------------------------------</div>
                     <div onClick={() => setIsActive(!isActive)} className="cursor-pointer">
                         <motion.div
                             className={`relative w-14 h-6 rounded-full flex items-center p-1`}
-                            initial={{ backgroundColor: "#E4E4E7" }}
+                            initial={{ backgroundColor: "#ffffff" }}
                             animate={{
-                            backgroundColor: isActive ? "#F97316" : "#E4E4E7",
+                            backgroundColor: isActive ? "#F97316" : "#ffffff",
                             }}
                             transition={{
                             type: "spring",
@@ -63,7 +64,7 @@ export default function ContactForm({ dark }: PropsType) {
                             }}
                         >
                             <motion.div
-                            className="w-4 h-4 bg-white rounded-full"
+                            className="w-4 h-4 bg-zinc-200 rounded-full"
                             initial={{ x: 0 }}
                             animate={{
                                 x: isActive ? 31.5 : 0,
@@ -77,24 +78,24 @@ export default function ContactForm({ dark }: PropsType) {
                         </motion.div>
         </div>
                 </div>
-                <motion.button className={`${dark ? "bg-zinc-900 text-zinc-400" : "bg-zinc-200"} text-zinc-600 rounded-lg px-2 py-1 w-fit`}
+                <motion.button className={`${dark ? "bg-zinc-900 text-zinc-300" : "bg-white"} text-zinc-600 rounded-lg px-2 py-1 w-fit`}
                     whileHover={{ scale: 1.055, }}
                     transition={{ type: "spring", duration: 0.3 }}
                     onClick={eraseAll}
                 >Erase</motion.button>
             </motion.div>
-            <motion.div className="w-full flex flex-row gap-3"
+            <motion.div className="w-full flex flex-row gap-2"
                 initial={{ opacity: 0, y: 25 }}
                 transition={{ delay: 0.2 }}
                 whileInView={{opacity: 1, y: 0}}
             >
                 <div className="w-full flex flex-col">
                     <div className="text-left">Subject</div>
-                    <input className={`w-full mx-4 my-1 p-2 rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-zinc-200"}`} placeholder="Coffee chat?!" value={subject} onChange={(e) => setSubject(e.target.value)}></input>
+                    <input className={`shadow-inner w-full mx-4 my-1 p-2 rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-white"}`} placeholder="Coffee chat?!" value={subject} onChange={(e) => setSubject(e.target.value)}></input>
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="text-left">From</div>
-                    <input className={`w-full mx-4 my-1 p-2 rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-zinc-200"}`} placeholder="bot@ai.com" value={from} onChange={(e) => setFrom(e.target.value)}></input>
+                    <input className={`shadow-inner w-full mx-4 my-1 p-2 rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-white"}`} placeholder="bot@ai.com" value={from} onChange={(e) => setFrom(e.target.value)}></input>
                 </div>
             </motion.div>
             <motion.div className="flex flex-col"
@@ -103,7 +104,7 @@ export default function ContactForm({ dark }: PropsType) {
                 whileInView={{opacity: 1, y: 0}}
             >
                 <div className="text-left">Content</div>
-                <textarea className={`w-full mx-4 my-1 p-2 h-[100px] rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-zinc-200"}`} placeholder="Input" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+                <textarea className={`shadow-inner w-full mx-4 my-1 p-2 h-[100px] rounded-lg mx-auto ${dark ? "bg-zinc-900 text-orange-200" : "text-orange-600 bg-white"}`} placeholder="Input" value={text} onChange={(e) => setText(e.target.value)}></textarea>
             </motion.div>
             <motion.button className="transition delay-150 ease-in-out hover:text-orange-400 w-fit mx-auto" onClick={() => sendEmail({ subject, from, text })}>Send</motion.button>
         </motion.main>
